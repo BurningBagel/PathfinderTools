7797984c8b7e2650eda3f90c3762df9fa741c240
Luiz Duarte <luiz.duarte11@yahoo.com.br>
2026-02-24 17:01:36 -0300

    .

---
+++ frontend/src/components/CraftingButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="crafting-button"
+    :class="{active,pressed}"
+    @mousedown="pressed = true"
+    @mouseup="pressed = false"
+    @mouseleave="pressed = false"
+    @click="$emit('click')"
+    >
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+
+    import { ref } from 'vue'
+
+    defineProps({
+        active: Boolean
+    })
+
+    const pressed = ref(false)
+
+</script>
+
+
+
+<!--  https://pixabay.com/sound-effects/film-special-effects-steampunk-mechanical-gadget-188052/ sound effect -->
+
+<!-- https://pixabay.com/sound-effects/film-special-effects-mechanical-push-switcher-393300/ -->
+
+<!-- https://pixabay.com/sound-effects/film-special-effects-cassette-recorder-stop-button-mechanical-click-sound-359987/ -->
